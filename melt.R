6f861958b710231b6bda40f1e0744c53b3539c3f
allthesignals <wilburnforce@gmail.com>
2017-11-14 11:32:13 -0500

    Use function, split into separate files

--- melt.R
+++ melt.R
@@ -1,20 +1,58 @@
 library(readxl)
 library(tidyr)
+library(plyr)
 library(reshape2)
 setwd("~/r-acs-munge")
 
-dataset <- read.csv("~/r-acs-munge/ACSDatabase_0610-1115.csv")
+melt_acs_data <- function(dataset, combined, metacols, name) {
+  df <- dataset[,combined]
+  melted <- melt(df, id.var=metacols)
+  split <- melted %>% separate(variable, into = c("variable", "type"), sep = -2)
+  casted <- dcast(split, GeoType + GeogName + GeoID + Dataset + variable ~ type)
+  write.csv(arrange(casted, desc(GeoID)), paste(name, '.csv', sep=''), na="", row.names = FALSE)
+}
 
+dataset <- read_excel("~/r-acs-munge/ACSDatabase_0610-1115.xlsx", skip = 1)
 data_dictionary <- read_excel("~/r-acs-munge/data_dictionary.xlsx")
+metacols <- c("PrdctType", "Dataset", "GeoType", "GeogName", "GeoID", "FIPS", "BoroID", "CT", "BoroCT1", "BoroCT2", "NTA_Equiv", "PUMA_Equiv")
 
-metacols <- c("PrdctType", "Year", "GeoType", "GeogName", "GeoID", "FIPS", "BoroID", "CT", "BoroCT1", "BoroCT2", "NTA_Equiv", "PUMA_Equiv")
+melt_acs_data(
+  dataset, 
+  c(
+    metacols, 
+    (data_dictionary[data_dictionary$Profile == 'Demographic' & data_dictionary$Category != 'Demographic- Special Variable',] %>% drop_na(VariableName))$VariableName
+  ), 
+  metacols, 
+  'demographic'
+)
 
-cols_from_meta <- data_dictionary[data_dictionary$Profile == 'Demographic' & data_dictionary$Category != 'Demographic- Special Variable',] %>% drop_na(VariableName)
+melt_acs_data(
+  dataset, 
+  c(
+    metacols, 
+    (data_dictionary[data_dictionary$Profile == 'Social' & data_dictionary$Category != 'Social-Special Variable',] %>% drop_na(VariableName))$VariableName
+  ),
+  metacols,
+  'social'
+)
 
-combined <- c(metacols, cols_from_meta$VariableName)
+melt_acs_data(
+  dataset, 
+  c(
+    metacols, 
+    (data_dictionary[data_dictionary$Profile == 'Economic' & data_dictionary$Category != 'Economic- Special Variable' ,] %>% drop_na(VariableName))$VariableName
+  ),
+  metacols,
+  'economic'
+)
 
-df <- dataset[,combined]
-melted <- melt(df, id.var=metacols)
-split <- melted %>% separate(variable, into = c("variable", "type"), sep = -2)
-casted <- dcast(split, GeoType + GeogName + GeoID + Year + variable ~ type)
-write.csv(casted, 'melted_full.csv', na="", row.names = FALSE)
+melt_acs_data(
+  dataset, 
+  c(
+    metacols, 
+    (data_dictionary[data_dictionary$Profile == 'Housing' & data_dictionary$Category != 'Housing- Special Variable',] %>% drop_na(VariableName))$VariableName
+  ),
+  metacols,
+  'housing'
+)
+
